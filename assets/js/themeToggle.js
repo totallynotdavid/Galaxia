759896ab884857c77fb2d8a80126f8921ceec23e
David Duran <dadch1404@gmail.com>
2024-06-08 00:21:38 -0500

    refactor: Improve theme toggle functionality and code structure

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -1,23 +1,31 @@
 let themeToggle = document.getElementById("themeToggle");
 
-themeToggle.addEventListener("click", function() {
+themeToggle.addEventListener("click", function () {
     let root = document.documentElement;
-    
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        root.classList.remove('dark');
-        localStorage.theme = 'light';
+
+    if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+        root.classList.remove("dark");
+        localStorage.theme = "light";
     } else {
-        root.classList.add('dark');
-        localStorage.theme = 'dark';
+        root.classList.add("dark");
+        localStorage.theme = "dark";
     }
 });
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
+document.addEventListener("DOMContentLoaded", event => {
+    if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+        document.documentElement.classList.add("dark");
     } else {
-        document.documentElement.classList.remove('dark');
+        document.documentElement.classList.remove("dark");
     }
 });
 
-console.log("themeToggle.js loaded")
+console.log("themeToggle.js loaded");
