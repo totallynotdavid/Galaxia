df79b3a1597014e235b5290feb7d701f0a244412
David Duran <dadch1404@gmail.com>
2024-06-09 02:58:30 -0500

    feat: Update font to Hanken Grotesk Variable and fix related paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-    content: ['./layouts/**/*.html', './content/**/*.md'],
+    content: ["./layouts/**/*.html", "./content/**/*.md"],
     darkMode: "class",
     theme: {
         extend: {
@@ -14,7 +14,7 @@ module.exports = {
                 14: "3.5rem"
             },
             fontFamily: {
-                sans: ['"Space Grotesk Variable"', ...fontFamily.sans]
+                sans: ['"Hanken Grotesk Variable"', ...fontFamily.sans]
             },
             colors: {
                 primary: colors.pink,
